b8027ad1e5e2cbdcdd25c3b6c62443275ae65c4c
Saurav Kumar <sauravkmr780@gmail.com>
2026-08-14 01:54:16 -0400

    updated correct url

--- test.config.ts
+++ test.config.ts
@@ -1,8 +1,8 @@
 export class TestConfig {
   //appUrl="http://localhost/opencart/upload/"
   //appUrl="https://naveenautomationlabs.com/opencart"
-  //appUrl="https://tutorialsninja.com/demo/"
-  appUrl = "https://naveenautomationlabs.com/opencart/";
+  appUrl="https://tutorialsninja.com/demo/"
+  //appUrl = "https://naveenautomationlabs.com/opencart/";
   //login credentails
   email = "sauravkmr780@gmail.com";
   password = "@1Infosys";
