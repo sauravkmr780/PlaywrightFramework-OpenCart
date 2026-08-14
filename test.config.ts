5dcd2f7a12a6f8d4d8977872a23fcdfedabc030d
Saurav Kumar <sauravkmr780@gmail.com>
2026-08-13 23:59:52 -0400

    Updated config file

--- test.config.ts
+++ test.config.ts
@@ -1,10 +1,13 @@
-export class TestConfig{
-appUrl="https://naveenautomationlabs.com/opencart/"
-//login credentails
-email="sauravkmr780@gmail.com"
-password="@1Infosys"
-//product details
-productName="MacBook"
-productQuantity="2"
-totalPrice="$1,204.00"
+export class TestConfig {
+  //appUrl="http://localhost/opencart/upload/"
+  //appUrl="https://naveenautomationlabs.com/opencart"
+  //appUrl="https://tutorialsninja.com/demo/"
+  appUrl = "https://naveenautomationlabs.com/opencart";
+  //login credentails
+  email = "sauravkmr780@gmail.com";
+  password = "@1Infosys";
+  //product details
+  productName = "MacBook";
+  productQuantity = "2";
+  totalPrice = "$1,204.00";
 }
